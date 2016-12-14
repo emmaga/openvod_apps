2efab4b343a6ce657b49d4a6e4c939ca0a6e57c0
clear-chenxiaobin <chenxiaobin@cleartv.cn>
2016-12-14 10:57:49 +0800

    应用组合

--- src/modules/controllers.js
+++ src/modules/controllers.js
@@ -76,13 +76,30 @@
                     util.setParams('token', '');
                     $state.go('login');
                 }
-                
+
                 self.goAppsGroup = function () {
                     $state.go('app.appsGroup');
                 }
 
                 self.goAppsWarehouse = function () {
                     $state.go('app.appsWarehouse');
+                }
+
+                /**
+                 * 添加 删除 弹窗，增加一个样式的class
+                 * @param bool
+                 * @param url
+                 */
+                self.showHideMask = function (bool, url) {
+                    // bool 为true时，弹窗出现
+                    if (bool) {
+                        $scope.app.maskUrl = url;
+                        $scope.app.showMaskClass = true;
+                    } else {
+                        $scope.app.maskUrl = '';
+                        $scope.app.showMaskClass = false;
+                    }
+
                 }
             }
         ])
@@ -95,8 +112,16 @@
                     {"groupId": 1, "groupName": "test_group2"},
                     {"groupId": 2, "groupName": "test_group3"}
                 ];
-                self.init = function() {
+                self.init = function () {
 
+                }
+
+                /**
+                 * 添加广告
+                 */
+                self.addAdv = function () {
+                    $scope.app.maskParams = {'groupId': self.groupId};
+                    $scope.app.showHideMask(true, 'pages/droupAdd.html');
                 }
             }
         ])
@@ -104,7 +129,59 @@
         .controller('appsGroupInfoController', ['$http', '$scope', '$state', '$filter', '$stateParams', 'NgTableParams', 'util',
             function ($http, $scope, $state, $filter, $stateParams, NgTableParams, util) {
                 var self = this;
-                self.init = function() {
+                self.init = function () {
+                    self.lang = util.langStyle();
+                    // self.defaultLangCode = util.getDefaultLangCode();
+                    self.groupId = $stateParams.groupId;
+                }
+
+                /**
+                 * 添加应用
+                 */
+                self.addApp = function () {
+                    $scope.app.maskParams = {'groupId': self.groupId};
+                    $scope.app.showHideMask(true, 'pages/appsImport.html');
+                }
+
+                /**
+                 * 删除应用
+                 * @param appId
+                 */
+                self.deleteApp = function (appId) {
+                    if (confirm('确定删除该应用吗？')) {
+                        self.deleting = true;
+                        var data = JSON.stringify({
+                            appId: appId
+                        })
+                        $http({
+                            method: 'POST',
+                            url: util.getApiUrl('logon', '', 'server'),
+                            data: data
+                        }).then(function successCallback(response) {
+                            var msg = response.data;
+                            if (msg.rescode == '200') {
+
+                            } else if (msg.rescode == "401") {
+                                alert('访问超时，请重新登录');
+                                $state.go('login')
+                            } else {
+                                alert(msg.rescode + ' ' + msg.errInfo);
+                            }
+                        }, function errorCallback(response) {
+                            alert(response.status + ' 服务器出错');
+                        }).finally(function (value) {
+                            self.deleting = false;
+                        });
+                    }
+                }
+            }
+        ])
+
+        .controller('appsImportController', ['$http', '$scope', '$state', '$filter', '$stateParams', 'NgTableParams', 'util',
+            function ($http, $scope, $state, $filter, $stateParams, NgTableParams, util) {
+                var self = this;
+                self.init = function () {
+                    self.groupId = $scope.app.maskParams.groupId;
                 }
             }
         ])
@@ -112,7 +189,7 @@
         .controller('appsWarehouseController', ['$http', '$scope', '$state', '$filter', '$stateParams', 'NgTableParams', 'util',
             function ($http, $scope, $state, $filter, $stateParams, NgTableParams, util) {
                 var self = this;
-                self.init = function() {
+                self.init = function () {
 
                 }
             }
